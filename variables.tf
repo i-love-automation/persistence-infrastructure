30893298543619cbd54e0b629083166885d6afbd
Terraform <romain.cambonie@gmail.com>
2023-04-18 09:14:04 +0200

    feat: adding terraform aws variables file

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "private_subnets_id" {
+  default = ""
+}
+
+variable "vpc_id" {
+  default = ""
+}
+
+variable "db_username" {
+  default = ""
+}
+
+variable "db_password" {
+  default = ""
+}
